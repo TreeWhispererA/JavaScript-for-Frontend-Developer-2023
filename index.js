139842de4c07f4872e36a09a5fb4c1670d8f075e
TreeWhispererA <139618051+TreeWhispererA@users.noreply.github.com>
2023-07-24 11:10:01 +0900

    1 file modified

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ let arr = [
   { id: 1, age: 12, name: 'Manu' },
   { id: 2, age: 24, name: 'Quincy' },
   { id: 3, age: 22, name: 'Abbey' },
-]
+];
 
-let names = arr.map(el => el.name);
-console.log(names)
+let names = arr.forEach((el) => {
+  el.age += 10;
+});
+console.log(arr);
+// forEach is similiar with map but it doesn't return ARRAY
